a6873a1fa52613bc197bce29f6589d51b9698277
root <anielvillegas@gmail.com>
2024-08-21 17:50:12 +0000

    update test-connection.py

--- src/test-connection.py
+++ src/test-connection.py
@@ -15,9 +15,9 @@ instance_name = os.environ["INSTANCE_NAME"]
 # initialize parameters
 INSTANCE_CONNECTION_NAME = f"{project_id}:{region}:{instance_name}" # i.e demo-project:us-central1:demo-instance
 print(f"Your instance connection name is: {INSTANCE_CONNECTION_NAME}")
-DB_USER = "an"
-DB_PASS = "qr4nIeL  "
-DB_NAME = "pgvec-db"
+DB_USER = os.environ["DB_USER"]
+DB_PASS = os.environ["DB_PASS"]
+DB_NAME = os.environ["DB_NAME"]
 
 def embed_text(
     texts: List[str] = ["banana muffins? ", "banana bread? banana muffins?"],
@@ -56,31 +56,6 @@ pool = sqlalchemy.create_engine(
 
 # connect to connection pool
 with pool.connect() as db_conn:
-  # create ratings table in our sandwiches database
-#   db_conn.execute(
-#     sqlalchemy.text(
-#       "CREATE TABLE IF NOT EXISTS ratings "
-#       "( id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, "
-#       "origin VARCHAR(255) NOT NULL, rating FLOAT NOT NULL, "
-#       "PRIMARY KEY (id));"
-#     )
-#   )
-
-#   # commit transaction (SQLAlchemy v2.X.X is commit as you go)
-#   db_conn.commit()
-
-#   # insert data into our ratings table
-#   insert_stmt = sqlalchemy.text(
-#       "INSERT INTO ratings (name, origin, rating) VALUES (:name, :origin, :rating)",
-#   )
-
-#   # insert entries into table
-#   db_conn.execute(insert_stmt, parameters={"name": "HOTDOG", "origin": "Germany", "rating": 7.5})
-#   db_conn.execute(insert_stmt, parameters={"name": "BÀNH MÌ", "origin": "Vietnam", "rating": 9.1})
-#   db_conn.execute(insert_stmt, parameters={"name": "CROQUE MADAME", "origin": "France", "rating": 8.3})
-
-#   # commit transactions
-#   db_conn.commit()
 
   # query and fetch ratings table
 #   results = db_conn.execute(sqlalchemy.text("SELECT * FROM ratings")).fetchall()
